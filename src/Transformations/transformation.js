ac56cd1cebd489e190588f15df359d49925b76b4
Trimpl <garfj2002@yandex.by>
2021-10-16 12:38:13 +0300

    asd

--- src/Transformations/transformation.js
+++ src/Transformations/transformation.js
@@ -1,45 +1,5 @@
 export const transformation = {
-    //просто перегон из координат в матрицу
-    getDefaultMatrix: (X, Y, Z, F) => {
-        return [[X, Y, Z, F]];
-    },
-
-    //поворот относительно X
-    getRotateMatrixRelativeX: (angle) => {
-        return [[1, 0, 0, 0], [0, Math.cos(angle), Math.sin(angle), 0], [0, -Math.sin(angle), Math.cos(angle), 0], [0, 0, 0, 1]]
-    },
-
-    //поворот относительно Y
-    getRotateMatrixRelativeY: (angle) => {
-        return [[Math.cos(angle), 0, -Math.sin(angle), 0], [0, 1, 0, 0], [Math.sin(angle), 0, Math.cos(angle), 0], [0, 0, 0, 1]]
-    },
-
-    //поворот относительно Z
-    getRotateMatrixRelativeZ: (angle) => {
-        return [[Math.cos(angle), Math.sin(angle), 0, 0], [-Math.sin(angle), Math.cos(angle), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
-    },
-
-    //перемещение
-    getMoveMatrix: (dx, dy, dz) => {
-        return [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [dx, dy, dz, 1]];
-    },
-
-    // Масштaб
-    getScaleMatrix: (sx, sy, sz) => {
-        return [[sx, 0, 0, 0], [0, sy, 0, 0], [0, 0, sz, 0], [0, 0, 0, 1]];
-    },
-
-    //Горизонтальная проекция
-    getHorizontalMatrix: () => {
-        return [[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]];
-    },
-    //вертикльная проекция
-    getProfileMatrix: () => {
-        return [[0, 0, 1, 0], [0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1]];
-    },
-    //умножение матриц
-    multiplyMatrix : (A, B) => {
-        debugger
+    multiplyMatrix: (A, B) => {
         let rowsA = A.length, colsA = A[0].length,
             rowsB = B.length, colsB = B[0].length,
             C = [];
@@ -53,8 +13,52 @@ export const transformation = {
             }
         }
         return C;
-    }
+    },
 
+    //Горизонтальная проекция
+    getHorizontalMatrix: () => {
+        return [[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]];
+    },
+    //вертикльная проекция
+    getProfileMatrix: () => {
+        return [[0, 0, 1, 0], [0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1]];
+    },
+
+    getRotateMatrixOx: (angle) => {
+        return [
+            [1, 0, 0, 0],
+            [0, Math.cos(angle), Math.sin(angle), 0],
+            [0, -Math.sin(angle), Math.cos(angle), 0],
+            [0, 0, 0, 1]];
+    },
+
+    getRotateMatrixOy: (angle) => {
+        return [
+            [Math.cos(angle), 0, -Math.sin(angle), 0],
+            [0, 1, 0, 0],
+            [Math.sin(angle), 0, Math.cos(angle), 0],
+            [0, 0, 0, 1]];
+    },
+
+    getRotateMatrixOz: (angle) => {
+        return [
+            [Math.cos(angle), Math.sin(angle), 0, 0],
+            [-Math.sin(angle), Math.cos(angle), 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]];
+    },
+
+    getMoveMatrix: (dx, dy, dz) => {
+        return [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [dx, dy, dz, 1]];
+    },
+
+    getResizeMatrix: (sx, sy, sz) => {
+        return [[sx, 0, 0, 0], [0, sy, 0, 0], [0, 0, sz, 0], [0, 0, 0, 1]];
+    },
+
+    getDefaultMatrix: (p) => {
+        return [[p.x, p.y, p.z, 1]];
+    },
 }
 
 
